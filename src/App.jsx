5c211b7d7ca09b9189104425847c390625390aa4
Brian Almada <brianalexisalmada84@gmail.com>
2023-07-12 20:46:45 -0300

    Crear componente hijo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import React, { useState } from 'react'
-import imagen0 from './img/rey_atanagildo.png';
-import imagen1 from './img/rey_ataulfo.png';
 import { useRef } from 'react';
 import './App.css'
 
