87c83bf3efb96fa85ca9e8825cdb299ab80fc0a0
Brian Almada <brianalexisalmada84@gmail.com>
2023-07-15 20:52:25 -0300

    crear constante y hooks

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,34 @@ import { useRef } from 'react';
 import './App.css';
 
 function App() {
-
+  const reyes=[
+    {
+      nombre:"Atanagildo",
+      color:"darkolivegreen",
+      precio:178
+    },{
+      nombre:"Ervigio",
+      color:"crimson",
+      precio:169
+    },{
+      nombre:"Ataúlfo",
+      color:"peru",
+      precio:81
+    },{
+      nombre:"Leogivildo",
+      color:"darkmagenta",
+      precio:126
+    },{
+      nombre:"Recesvinto",
+      color:"royalblue",
+      precio:141
+    },{
+      nombre:"Sisebuto",
+      color:"teal",
+      precio:69
+    }
+  ]
+  const [total, setTotal] = useState(0);
   return (
     <>
     <ReyAtanagildo />
